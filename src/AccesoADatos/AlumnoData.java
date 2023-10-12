55fe5560e8af11b42671bafabede28c6ee0a5970
Hernan Funes <funes.hernan@hotmail.es>
2023-10-12 19:35:48 -0300

    creado AlumnoData

---
+++ src/AccesoADatos/AlumnoData.java
@@ -0,0 +1,24 @@
+
+package AccesoADatos;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Hernán Funes
+ */
+public class AlumnoData {
+    private Connection con = null;
+    
+    public void guardarAlumno(){
+    
+    }
+    
+//    public Alumno buscarAlumno(){
+//        
+//    }
+    
+//    public Alumno buscarAlumnoPorDni(int dni){
+//        
+//    }
+}
