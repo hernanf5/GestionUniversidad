231f0b53374de20fcfcc6451ed4ff57999be8cdf
Hernan Funes <funes.hernan@hotmail.es>
2023-10-23 19:42:19 -0300

    index refactor
    Se movio el main fraim al paquete de vistas

--- src/Vistas/Index.java
+++ src/Vistas/Index.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package AccesoADatos;
+package Vistas;
 
 import java.awt.Image;
 import javax.swing.ImageIcon;
